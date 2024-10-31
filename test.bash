5e6aed8388e1ec070664430fc7b7fa36d36af372
Yuna Isomura <yuna.i.200446@gmail.com>
2024-10-31 12:20:03 +0900

    add command

--- test.bash
+++ test.bash
@@ -6,8 +6,9 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
