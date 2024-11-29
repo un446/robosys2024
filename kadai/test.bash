696d07e0fb08c66b2e03e5e50f17031975703a7f
Yuna Isomura <yuna.i.200446@gmail.com>
2024-11-29 19:13:06 +0900

    update test

--- kadai/test.bash
+++ kadai/test.bash
@@ -3,23 +3,24 @@
 #SPDX-License-Identifier:BSD-3-Clause
 
 ng () {
-    echo "${1}行目が違うよ"
+    echo NG at Line $1
         res=1
 }
 
 res=0
 
 out=$(./fibo 1)
-out=$(echo "$out" | xargs)
-[ "${out}" = "1" ] || ng "$LINENO"
+out=$(echo "${out}" | xargs)
+[ "${out}" = "1" ] || ng $LINENO
 
 out=$(./fibo 5)
-out=$(echo "$out" | xargs)
-[ "${out}" = "1 1 2 3 5" ] || ng "$LINENO"
+out=$(echo "${out}" | xargs)
+[ "${out}" = "1 1 2 3 5" ] || ng $LINENO
 
 out=$(./fibo 10)
-out=$(echo "$out" | xargs)
-[ "${out}" = "1 1 2 3 5 8 13 21 34 55" ] || ng "$LINENO"
+out=$(echo "${out}" | xargs)
+[ "${out}" = "1 1 2 3 5 8 13 21 34 55" ] || ng $LINENO
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
