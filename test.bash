4aba06452e4ffa9203598666da7ad7197cb105cf
Yuna Isomura <yuna.i.200446@gmail.com>
2024-12-04 01:27:47 +0900

    add README.md

--- test.bash
+++ test.bash
@@ -9,17 +9,20 @@ ng(){
 
 res=0
 
+
+### I/O ###
 out=$(./fibo 1)
 out=$(echo "$out" | xargs)
-[ "${out}" = "1" ] || ng "$LINENO"
+[ "${out}" = "1" ] || ng "${LINENO}"
 
 out=$(./fibo 5)
 out=$(echo "$out" | xargs)
-[ "${out}" = "1 1 2 3 5" ] || ng "$LINENO"
+[ "${out}" = "1 1 2 3 5" ] || ng "${LINENO}"
 
 out=$(./fibo 10)
 out=$(echo "$out" | xargs)
-[ "${out}" = "1 1 2 3 5 8 13 21 34 55" ] || ng "$LINENO"
+[ "${out}" = "1 1 2 3 5 8 13 21 34 55" ] || ng "${LINENO}"
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
